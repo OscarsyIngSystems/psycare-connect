d871cfa01100c10327cd34e8d92030aa4d53e614
Oscar Serrano <oscarsy.ing.systems@hotmail.com>
2025-12-11 20:28:43 -0600

    Se agregan imagenes nuevas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DetallesComponent } from '../components/detalles/detalles.component';
 import { DirectorioComponent } from '../components/directorio/directorio.component';
+import { AdministradorComponent } from '../components/administrador/administrador.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'detalles',
     component: DetallesComponent,
+  },
+  {
+    path: 'admin',
+    component: AdministradorComponent,
   },
   {
     path: '**',
