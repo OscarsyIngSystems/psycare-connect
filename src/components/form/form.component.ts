72e611f2a9ec73bd95cf9c4fa59d3481e9734d23
Daniel David Perez Coapango <dano.prez.coa@gmail.com>
2025-12-14 22:08:48 -0600

    Formulario para edicion/nuevo

--- src/components/form/form.component.ts
+++ src/components/form/form.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      direccion: [''],
+      contacto: [''],
+      correo: [''],
+      iframe: ['', Validators.required],
+      estado: ['1', Validators.required],
+      atencion: ['', Validators.required]
+    })
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
